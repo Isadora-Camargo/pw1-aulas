a137f094faac145d9dcea6264171d267e8356db0
Isadora <160609751+Isadora-Camargo@users.noreply.github.com>
2024-03-26 15:42:40 -0300

    Delete Isa-PW/exercicio06 directory

--- Isa-PW/exercicio06/assets/scripts.js
+++
@@ -1,7 +0,0 @@
-const item = document.querySelector("#item");
-const button = document.querySelector("#myButtonInsert");
-const ul = document.querySelector("#myList");
-
-button.addEventListener("click", () => {
-  ul.innerHTML += `<li>${item.value}</li>`;
-});
