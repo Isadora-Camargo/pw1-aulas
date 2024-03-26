986c1f4a6cf75681261682ae2f4ab927a437c124
Isadora <160609751+Isadora-Camargo@users.noreply.github.com>
2024-03-26 15:42:01 -0300

    Delete Isa-PW/exemplo07 directory

--- Isa-PW/exemplo07/assets/scripts.js
+++
@@ -1,6 +0,0 @@
-let name = window.prompt("Informe seu nome: ");
-let age = Number(window.prompt("Informe sua idade: "));
-
-document.querySelector(
-  "#content"
-).innerHTML = `Olá, ${name}! Seja bem-vindo. Sua idade é ${age}`;
